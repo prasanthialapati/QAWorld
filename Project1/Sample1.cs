c9dcb12c16b3d7ba6e956f787a0880d4fd42a48b
prasanti <prasanti@LAPTOP-K0CTL891>
2020-12-28 18:35:45 -0500

    Add project files.

---
+++ Project1/Sample1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Sample1
+    {
+        [Test]
+        public void launchBrowser()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https:www.google.com");
+        }
+    }
+}
